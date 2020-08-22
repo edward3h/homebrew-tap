aaa46951e834f83ad9a68f8600eb477f0c704fbc
Edward Harman <eharman@tripadvisor.com>
2020-08-22 13:28:08 -0400

    attempt to fix for brew test bot

--- Formula/mc-pack-installer.rb
+++ Formula/mc-pack-installer.rb
@@ -1,18 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class McPackInstaller < Formula
-  desc "work in progress, don't try to use it uet"
+  desc "Minecraft Addon Pack Installer"
   homepage "https://github.com/edward3h/mc-pack-installer"
   url "https://github.com/edward3h/mc-pack-installer/archive/v1.0.0-rc1.tar.gz"
   sha256 "3b56cfa90ff85c6d4cc05dfe6e76959c936737fbf29def3a8e4e1dd598b41c38"
   license "GPL-3.0-only"
 
-  depends_on :java => "1.8"
+  depends_on java: "1.8"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
     system "./gradlew", "installDist"
     bin.install "build/install/mc-pack-installer/bin/mc-pack-installer"
     lib.install Dir["build/install/mc-pack-installer/lib/*"]
